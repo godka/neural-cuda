5e959e082c4f2fedce8d6892212f80c1f6de7c78
godka <asdic182@sina.com>
2016-09-28 14:14:15 +0800

    基本搞定

---
+++ Release/neural-cuda.h
@@ -0,0 +1,21 @@
+#pragma once
+//#include <Windows.h>
+#define MYTHAPI _stdcall
+#ifdef __cplusplus   
+#define HBAPI extern "C" __declspec (dllimport)   
+#else   
+#define HBAPI __declspec (dllimport)   
+#endif   
+
+HBAPI int MYTHAPI cuda_reciprocal_double(double *dst, double *src, unsigned int size, double scale);
+HBAPI int MYTHAPI cuda_reciprocal_float(float *dst, float *src, unsigned int size, float scale);
+
+HBAPI int MYTHAPI cuda_addnumber_double(double *dst, double *src, unsigned int size, double v,double scale);
+HBAPI int MYTHAPI cuda_addnumber_float(float *dst, float *src, unsigned int size, float v,float scale);
+
+HBAPI int MYTHAPI  cuda_dsigmoid(double *A, double *B, unsigned int size);
+HBAPI int MYTHAPI  cuda_hadamardProduct(const double *A, const double *B, double *R, unsigned int size);
+HBAPI int MYTHAPI  cuda_sigmoid(double *A, double *B, unsigned int size);
+HBAPI int MYTHAPI  cuda_dsigmoid(double *A, double *B, unsigned int size);
+HBAPI int MYTHAPI  cuda_exp(double *A, double *B, unsigned int size);
+HBAPI int MYTHAPI  cuda_test();
