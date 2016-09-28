b45463c792d0b4de9c9e719203073e59a71e6cfd
scarsty <scarsty@gmail.com>
2016-09-28 17:16:49 +0800

    no message

--- neural-cuda/kernel.cu
+++ neural-cuda/kernel.cu
@@ -5,205 +5,70 @@
 #include "math_functions.hpp"
 #undef __CUDA_INTERNAL_COMPILATION__
 #include <stdio.h>
-#include "neural-cuda.h"
+#include "will_cuda.h"
 #define blockMax 1024
-__global__ void AddNumberDoubleKernel(double* dst, double* src, double v,double scale, int N)
+
+#define cal_i() (blockIdx.x * blockDim.x + threadIdx.x)
+
+inline int blockNum(unsigned int size) { return (size + blockMax - 1) / blockMax; }
+
+inline int getError(const char* content)
 {
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < N)
-		dst[i] = v + scale * src[i];
+    cudaError_t cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess)
+    {
+        fprintf(stderr, "%s Kernel launch failed: %s\n", content, cudaGetErrorString(cudaStatus));
+        return 1;
+    }
+    return 0;
 }
+
+__global__ void AddNumberDoubleKernel(double* dst, double* src, double v, double scale, int N)
+{
+    int i = cal_i();
+    if (i < N) { dst[i] = v + scale * src[i]; }
+}
+
 __global__ void AddNumberSingleKernel(float* dst, float* src, float v, float scale, int N)
 {
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < N)
-		dst[i] = v + scale * src[i];
+    int i = cal_i();
+    if (i < N) { dst[i] = v + scale * src[i]; }
 }
+
 __global__ void ReciprocalDoubleKernel(double* dst, double* src, double scale, int N)
 {
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < N)
-		dst[i] = scale / src[i];
+    int i = cal_i();
+    if (i < N) { dst[i] = scale / src[i]; }
 }
+
 __global__ void ReciprocalSingleKernel(float* dst, float* src, float scale, int N)
 {
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < N)
-		dst[i] = scale / src[i];
+    int i = cal_i();
+    if (i < N) { dst[i] = scale / src[i]; }
 }
-__global__ void MatrixMulKernel(const double* A, const double* B, double* C, int N)
+
+
+
+HBAPI int MYTHAPI cuda_addnumber(double *dst, double *src, unsigned int size, double v, double scale)
+{ 
+    AddNumberDoubleKernel << < blockNum(size), blockMax >> >(dst, src, v, scale, size);
+    return getError("add number double");
+}
+
+HBAPI int MYTHAPI cuda_addnumber(float *dst, float *src, unsigned int size, float v, float scale)
 {
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < N)
-		C[i] = A[i] * B[i];
+    AddNumberSingleKernel << < blockNum(size), blockMax >> >(dst, src, v, scale, size);
+    return getError("add number float");
 }
 
-__global__ void SigmoidKernel(double* A, double* B, int N)
+HBAPI int MYTHAPI cuda_reciprocal(float *dst, float *src, unsigned int size, float scale)
 {
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < N)
-	{
-		B[i] = 1 / (1 + exp(-A[i]));
-	}
+    ReciprocalSingleKernel << < blockNum(size), blockMax >> >(dst, src, scale, size);
+    return getError("reciprocal number double");
 }
-__global__ void DsigmoidKernel(double* A, double* B, int N)
+
+HBAPI int MYTHAPI cuda_reciprocal(double *dst, double *src, unsigned int size, double scale)
 {
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < N)
-	{
-		double a = 1 + exp(-A[i]);
-		B[i] = (a - 1) / (a*a);
-	}
-}
-
-__global__ void ExpKernel(double* A, double* B, int N)
-{
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	if (i < N)
-	{
-		B[i] = exp(A[i]);
-	}
-}
-
-HBAPI int MYTHAPI cuda_hadamardProduct(const double *A, const double *B, double *R, unsigned int size)
-{
-	int blockNum = (size + blockMax - 1) / blockMax;
-
-	MatrixMulKernel << < blockNum, blockMax >> >(A, B, R, size);
-	cudaError_t cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		return 1;
-	}
-	//cudaStatus = cudaDeviceSynchronize();
-	//if (cudaStatus != cudaSuccess) {
-	//	fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-	//	return cudaStatus;
-	//}
-	return 0;
-}
-
-HBAPI int MYTHAPI cuda_addnumber_double(double *dst, double *src, unsigned int size, double v, double scale){
-	int blockNum = (size + blockMax - 1) / blockMax;
-
-	AddNumberDoubleKernel << < blockNum, blockMax >> >(dst, src, v,scale, size);
-	cudaError_t cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		return 1;
-	}
-	//cudaStatus = cudaDeviceSynchronize();
-	//if (cudaStatus != cudaSuccess) {
-	//	fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-	//	return cudaStatus;
-	//}
-	return 0;
-}
-HBAPI int MYTHAPI cuda_addnumber_float(float *dst, float *src, unsigned int size, float v, float scale){
-	int blockNum = (size + blockMax - 1) / blockMax;
-
-	AddNumberSingleKernel << < blockNum, blockMax >> >(dst, src, v, scale, size);
-	cudaError_t cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		return 1;
-	}
-	//cudaStatus = cudaDeviceSynchronize();
-	//if (cudaStatus != cudaSuccess) {
-	//	fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-	//	return cudaStatus;
-	//}
-	return 0;
-}
-HBAPI int MYTHAPI cuda_reciprocal_float(float *dst, float *src, unsigned int size, float scale)
-{
-	int blockNum = (size + blockMax - 1) / blockMax;
-
-	ReciprocalSingleKernel << < blockNum, blockMax >> >(dst, src, scale, size);
-	cudaError_t cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		return 1;
-	}
-	//cudaStatus = cudaDeviceSynchronize();
-	//if (cudaStatus != cudaSuccess) {
-	//	fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-	//	return cudaStatus;
-	//}
-	return 0;
-
-}
-HBAPI int MYTHAPI cuda_reciprocal_double(double *dst, double *src, unsigned int size,double scale)
-{
-	int blockNum = (size + blockMax - 1) / blockMax;
-
-	ReciprocalDoubleKernel << < blockNum, blockMax >> >(dst, src, scale,size);
-	cudaError_t cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		return 1;
-	}
-	//cudaStatus = cudaDeviceSynchronize();
-	//if (cudaStatus != cudaSuccess) {
-	//	fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-	//	return cudaStatus;
-	//}
-	return 0;
-
-}
-
-HBAPI int MYTHAPI cuda_dsigmoid(double *A, double *B, unsigned int size)
-{
-	int blockNum = (size + blockMax - 1) / blockMax;
-	DsigmoidKernel << < blockNum, blockMax >> >(A, B, size);
-	cudaError_t cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		return 1;
-	}
-	//cudaStatus = cudaDeviceSynchronize();
-	//if (cudaStatus != cudaSuccess) {
-	//	fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-	//	return 1;
-	//}
-	return 0;
-}
-
-HBAPI int MYTHAPI  cuda_test(){
-	printf("hello cuda!\n");
-	return 0;
-}
-HBAPI int MYTHAPI cuda_sigmoid(double *A, double *B, unsigned int size)
-{
-	int blockNum = (size + blockMax - 1) / blockMax;
-
-	SigmoidKernel << < blockNum, blockMax >> >(A, B, size);
-	cudaError_t cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		return 1;
-	}
-	//cudaStatus = cudaDeviceSynchronize();
-	//if (cudaStatus != cudaSuccess) {
-	//	fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-	//	return 1;
-	//}
-	return 0;
-}
-HBAPI int MYTHAPI cuda_exp(double *A, double *B, unsigned int size)
-{
-	int blockNum = (size + blockMax - 1) / blockMax;
-
-	SigmoidKernel << < blockNum, blockMax >> >(A, B, size);
-	cudaError_t cudaStatus = cudaGetLastError();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-		return 1;
-	}
-	cudaStatus = cudaDeviceSynchronize();
-	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-		return 1;
-	}
-	return 0;
+    ReciprocalDoubleKernel << < blockNum(size), blockMax >> >(dst, src, scale, size);
+    return getError("reciprocal number float");
 }
--- neural-cuda/will_cuda.h
+++ neural-cuda/will_cuda.h
@@ -2,20 +2,22 @@
 //#include <Windows.h>
 #define MYTHAPI _stdcall
 #ifdef __cplusplus   
-#define HBAPI extern "C" __declspec (dllimport)   
+#define HBAPI /*extern "C"*/ __declspec (dllexport)   
 #else   
-#define HBAPI __declspec (dllimport)   
+#define HBAPI __declspec (dllexport)   
 #endif   
 
-HBAPI int MYTHAPI cuda_reciprocal_double(double *dst, double *src, unsigned int size, double scale);
-HBAPI int MYTHAPI cuda_reciprocal_float(float *dst, float *src, unsigned int size, float scale);
+HBAPI int MYTHAPI cuda_reciprocal(double *dst, double *src, unsigned int size, double scale);
+HBAPI int MYTHAPI cuda_reciprocal(float *dst, float *src, unsigned int size, float scale);
 
-HBAPI int MYTHAPI cuda_addnumber_double(double *dst, double *src, unsigned int size, double v,double scale);
-HBAPI int MYTHAPI cuda_addnumber_float(float *dst, float *src, unsigned int size, float v,float scale);
+HBAPI int MYTHAPI cuda_addnumber(double *dst, double *src, unsigned int size, double v,double scale);
+HBAPI int MYTHAPI cuda_addnumber(float *dst, float *src, unsigned int size, float v,float scale);
 
+/*
 HBAPI int MYTHAPI  cuda_dsigmoid(double *A, double *B, unsigned int size);
 HBAPI int MYTHAPI  cuda_hadamardProduct(const double *A, const double *B, double *R, unsigned int size);
 HBAPI int MYTHAPI  cuda_sigmoid(double *A, double *B, unsigned int size);
 HBAPI int MYTHAPI  cuda_dsigmoid(double *A, double *B, unsigned int size);
 HBAPI int MYTHAPI  cuda_exp(double *A, double *B, unsigned int size);
 HBAPI int MYTHAPI  cuda_test();
+*/
